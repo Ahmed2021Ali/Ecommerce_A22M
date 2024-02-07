1665a23c6b8019e276cd79847cebe005ae5e7e5e
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-02-07 10:28:11 +0200

    Update
    Update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -77,6 +77,6 @@ class AuthController extends Controller
                 ]);
             }
             Auth::login($user, true);
-            return to_route('homepage');
+            return redirect('home');
         }
 }
