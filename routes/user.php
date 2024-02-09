573a091dd836b5351ba2cc3061b619677ba90da5
Ahmed <uaahmed89@gmail>
2024-02-09 11:09:26 +0200

    add producs route

--- routes/user.php
+++ routes/user.php
@@ -21,30 +21,3 @@ Route::controller(HomepageController::class)->group(function(){
 
 
 Route::resource('products', ProductControlle::class);
-<<<<<<< HEAD
-=======
-
-Route::controller(FavController::class)->prefix('fav')->as('fav.')->group(function(){
-    Route::get('/index',  'index')->name('index');
-    Route::get('/store/{product}',  'store')->name('store');
-    Route::get('/destroy/{fav}',  'destroy')->name('destroy');
-});
-
-Route::controller(CartController::class)->prefix('cart')->as('cart.')->group(function(){
-    Route::get('/index',  'index')->name('index');
-    Route::post('/store/{product}',  'store')->name('store');
-    Route::put('/update/{cart}',  'store')->name('store');
-    Route::get('/destroy/{cart}',  'destroy')->name('destroy');
-});
-
-Route::controller(OrderController::class)->prefix('order')->as('order.')->group(function(){
-    Route::get('/index',  'index')->name('index');
-    Route::post('/store/{address}',  'store')->name('store');
-    Route::get('/destroy/{order}',  'destroy')->name('destroy');
-});
-Route::post('/contactUs', [ContactUsController::class, 'store'])->name('contact.store');
-
-
-Route::resource('address', AddressController::class);
-
->>>>>>> cd68e10b1e25aca7833fde02883ba6d5fc11a4e0
