abe8ddb3c4e0f92bfb629d56a38b6b55b957d018
Ahmed <uaahmed89@gmail>
2024-02-10 18:58:36 +0200

    erge remote changes into local branch

--- app/Http/Controllers/UserDashboard/ProfileController.php
+++ app/Http/Controllers/UserDashboard/ProfileController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\UserDashboard;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Profile\ProfileRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -11,4 +14,40 @@ class ProfileController extends Controller
     {
         return view('userDashboard.profile.index');
     }
+
+
+
+    public function update(ProfileRequest $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            $user = User::find(Auth::id());
+
+            if ($request->filled('password')) {
+                $validatedData['password'] = Hash::make($request->password);
+            } else {
+                // Remove the password key from the validated data to avoid updating it with an empty value
+                unset($validatedData['password']);
+            }
+
+            $updateResult = $user->update($validatedData);
+
+            if ($updateResult) {
+                toastr()->success('تم تحديث البيانات بنجاح');
+                return to_route('profile.index');
+            } else {
+                toastr()->error('لم يتم تحديث البيانات، حاول مرة أخرى.');
+                return back()->withErrors([
+                    'error' => 'لم يتم تحديث البيانات، حاول مرة أخرى.',
+                ])->withInput($validatedData);
+
+            }
+        } catch (\Exception $e) {
+            toastr()->error('حدث خطأ أثناء تحديث البيانات');
+            return back()->withErrors(['error' => 'حدث خطأ أثناء تحديث البيانات']);
+        }
+    }
+
+    
 }
