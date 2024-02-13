7e6b1450b457c42cb39a8f9621c4627ce584eeed
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-02-13 11:37:38 +0200

    update

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,20 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Banner extends Model implements HasMedia
+class Banner extends Model
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory;
 
     protected $fillable = ['status', 'small_title', 'main_title', 'product_id'];
 
-    public function registerMediaConversions(Media $media = null): void
-    {
-        $this->addMediaCollection('bannerFiles');
-    }
 
     public function product()
     {
