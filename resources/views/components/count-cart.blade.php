7a87744dadeacb9a660f6efdeff6483333f0ee77
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-02-14 13:15:42 +0200

    update
    update

--- resources/views/components/count-cart.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    @if( Auth::check())
-        <span class="pro-count blue">{{$countCart}}</span>
-    @endif
-</div>
--- resources/views/components/count-fav.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if( Auth::check())
-        <span class="pro-count blue">{{$countFav}}</span>
-    @endif
-</div>
-
-
