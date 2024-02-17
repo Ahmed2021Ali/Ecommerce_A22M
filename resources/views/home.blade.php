103582feba4a3e07e60400bce1d549b2b18ef98b
Ahmed Ali Abdulghani <92600564+Ahmed2021Ali@users.noreply.github.com>
2024-02-17 16:01:28 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
                             </button>
                         </li>
                     @endif
-                    @if(!$newAddedProducts->isEmpty())
+                    @if(!$newProducts->isEmpty())
                         <li class="nav-item" role="presentation">
                             <button class="nav-link" id="nav-tab-three" data-bs-toggle="tab" data-bs-target="#tab-three"
                                     type="button" role="tab" aria-controls="tab-three" aria-selected="false">المنتجات
@@ -62,7 +62,7 @@
                     @include('userDashboard.products.popular.index')
                 @endif
 
-                @if(!$newAddedProducts->isEmpty())
+                @if(!$newProducts->isEmpty())
                     @include('userDashboard.products.newAdded.index')
                 @endif
 
@@ -79,7 +79,9 @@
                                         <div class="card-1">
                                             <figure class="img-hover-scale overflow-hidden">
                                                 <a href="{{ route('category.products', encrypt($category->id)) }}">
-                                                    <img src="{{ $category->getFirstMediaUrl('categoryFiles') }}" alt="{{ $category->name }}" style="width: 100%; height: 150px; object-fit: cover;">
+                                                    <img src="{{ $category->getFirstMediaUrl('categoryFiles') }}"
+                                                         alt="{{ $category->name }}"
+                                                         style="width: 100%; height: 150px; object-fit: cover;">
                                                 </a>
                                             </figure>
                                             <h5>
@@ -100,56 +102,11 @@
         @include('userDashboard.banners.index')
     @endif
 
-    @if(!$newArrivalProducts->isEmpty())
-        <section class="section-padding" style="direction: rtl; text-align: center;">
-            <div class="container wow fadeIn animated">
-                <h3 class="section-title mb-20"><span>المنتجات</span> الجديدة</h3>
-                <div class="carausel-6-columns-cover position-relative">
-                    <div class="slider-arrow slider-arrow-2 carausel-6-columns-arrow"
-                         id="carausel-6-columns-2-arrows"></div>
-                    <div class="carausel-6-columns carausel-arrow-center" id="carausel-6-columns-2">
-                        @foreach($newArrivalProducts as $product)
-                            <div class="product-cart-wrap small hover-up">
-                                <div class="product-img-action-wrap">
-                                    <div class="product-img product-img-zoom"
-                                         style="direction: rtl; text-align: right;">
-                                        <a href="{{ route('products.show', encrypt($product->id)) }}">
-                                            <img src="{{ $product->getFirstMediaUrl('productFiles') }}" width="400" height="250" style="direction: rtl; text-align: right;">
-                                        </a>
-                                    </div>
-                                    <div class="product-action-1">
-                                        <a href="{{ $product->getFirstMediaUrl('productFiles') }}" aria-label="عرض" class="action-btn hover-up" data-bs-target="#quickViewModal"><i class="fi-rs-eye"></i></a>
-                                        <a id="addToFavoritesBtn" aria-label="إضافة إلي المفضلة" onclick="addToFavorites({{ $product->id }})"><i class="fi-rs-heart"></i></a>
-                                        <a aria-label="تسوق الآن" class="action-btn hover-up" href="{{ route('products.show', encrypt($product->id)) }}"><i class="fi-rs-shopping-bag-add"></i></a>
-                                        <a aria-label="تسوق الآن" class="action-btn hover-up" href="{{route('products.show', encrypt($product->id))}}"><i class="fi-rs-shopping-bag-add"></i></a>
-                                    </div>
-                                </div>
-                                <div class="product-content-wrap">
-                                    <h2>
-                                        <a href="{{ route('products.show', encrypt($product->id)) }}">{{ $product->name }}</a>
-                                    </h2>
-                                    {{ Str::limit($product->description, 35) }}
-                                    <span>
-                                    @include('userDashboard.products.review.ratingProduct',['rate'=>calcReview($product)])
-                                        @if($product->offer)
-                                            <span>تخفيض %{{ $product->offer }}</span>
-                                        @endif
-                                </span>
-                                    <div class="product-price">
-                                        <span>{{ $product->price_after_offer ?? $product->price }}</span>
-                                        @if($product->offer)
-                                            <span class="old-price">${{ $product->price }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- End product-cart-wrap-2 -->
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </section>
+    @if(!$newProducts->isEmpty())
+        @include('userDashboard.products.newArrival.index')
     @endif
+
+
 
     @if(!$brands->isEmpty())
         <section class="section-padding">
@@ -161,9 +118,10 @@
                          id="carausel-6-columns-3-arrows"></div>
                     <div class="carausel-6-columns text-center" id="carausel-6-columns-3">
                         @foreach($brands as $brand)
-                        <div class="brand-logo">
-                                <img src="{{ $brand->getFirstMediaUrl('brandFiles') }}" class="img-grey-hover" alt="الماركات" style="width: 175px; height: 150px;">
-                        </div>
+                            <div class="brand-logo">
+                                <img src="{{ $brand->getFirstMediaUrl('brandFiles') }}" class="img-grey-hover"
+                                     alt="الماركات" style="width: 175px; height: 150px;">
+                            </div>
                         @endforeach
                     </div>
                 </div>
