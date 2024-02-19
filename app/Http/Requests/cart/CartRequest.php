729429e11640c287c14433c9759f7164189b24af
Ahmed <uaahmed89@gmail>
2024-02-20 00:09:00 +0200

    Enhance all validation

--- app/Http/Requests/cart/CartRequest.php
+++ app/Http/Requests/cart/CartRequest.php
@@ -18,8 +18,8 @@ class CartRequest extends FormRequest
     {
         return [
             'quantity'=>['required','string','min:1','max:99'],
-            'color'=>['nullable'],
-            'size'=>['nullable'],
+            'color'=>['nullable', 'max:50'],
+            'size'=>['nullable', 'max:50'],
             'button'=>['required',Rule::in(['addCart', 'payNow'])],
         ];
     }
@@ -28,15 +28,14 @@ class CartRequest extends FormRequest
     {
         return [
             'quantity.required' => 'حقل الكمية مطلوب.',
-            'quantity.string' => 'يجب أن تكون الكمية رقمًا.',
-            'quantity.min' => 'حقل الكمية لا يقل عن واحد .',
-            'quantity.max' => 'حقل الكمية لا يزيد 99 منتج في المرة الواحدة .',
-
-            'color.nullable' => 'حقل الكمية مطلوب.',
-            'size.nullable' => 'حقل الكمية مطلوب.',
-
+            'quantity.string' => 'يجب أن يكون حقل الكمية نصًا.',
+            'quantity.min' => 'الحد الأدنى للكمية هو 1.',
+            'quantity.max' => 'الحد الأقصى للكمية هو 99.',
+            'color.max' => 'الحد الأقصى لللون هو 50.',
+            'size.max' => 'الحد الأقصى للحجم هو 50.',
             'button.required' => 'حقل الزر مطلوب.',
-            'button.in' => 'يجب أن يكون الزر إما "إضافة إلى السلة" أو "الدفع الآن".',
+            'button.in' => 'قيمة غير صالحة لحقل الزر. يجب أن تكون "إضافة إلى السلة" أو "الدفع الفوري".',
         ];
     }
+    
 }
--- app/Http/Requests/contact/ContactRequest.php
+++ app/Http/Requests/contact/ContactRequest.php
@@ -15,37 +15,36 @@ class ContactRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required','string','min:0','mix:50',
-            'email' => 'required','email',
+            'name' => 'required','string','min:5','mix:50',
+            'email' => 'required','email','mix:50',
             'phone' => 'required','numeric','min:4','max:20',
             'subject' => 'required','string','min:3','max:100',
             'message' => 'required','string','min:3','max:200',
         ];
     }
+
     public function messages()
     {
         return [
             'name.required' => 'حقل الاسم مطلوب.',
-            'name.mix' => 'الاسم الا يزيد عن 50 حرف.',
-
-
+            'name.string' => 'يجب أن يكون حقل الاسم نصًا.',
+            'name.min' => 'الحد الأدنى لطول الاسم هو 5 أحرف.',
+            'name.max' => 'الحد الأقصى لطول الاسم هو 50 حرفًا.',
             'email.required' => 'حقل البريد الإلكتروني مطلوب.',
-            'email.email' => 'يجب أن يكون البريد الإلكتروني صالحًا.',
-
+            'email.email' => 'البريد الإلكتروني يجب أن يكون عنوان بريد إلكتروني صالحًا.',
+            'email.max' => 'الحد الأقصى لطول البريد الإلكتروني هو 50 حرفًا.',
             'phone.required' => 'حقل الهاتف مطلوب.',
-            'phone.numeric' => 'يجب أن يكون الهاتف رقمًا.',
-            'phone.min' => 'يجب أن يتألف الهاتف من 11 رقمًا.',
-
+            'phone.numeric' => 'يجب أن يكون حقل الهاتف رقمًا.',
+            'phone.min' => 'الحد الأدنى لطول رقم الهاتف هو 4 أرقام.',
+            'phone.max' => 'الحد الأقصى لطول رقم الهاتف هو 20 رقمًا.',
             'subject.required' => 'حقل الموضوع مطلوب.',
-            'subject.string' => 'يجب أن يكون الموضوع نصًا.',
-            'subject.mix' => 'الموضوع الا يقل عن 3 احرف.',
-            'subject.min' => ' الموضوع الا يقل عن 100 احرف.',
-
-
+            'subject.string' => 'يجب أن يكون حقل الموضوع نصًا.',
+            'subject.min' => 'الحد الأدنى لطول الموضوع هو 3 أحرف.',
+            'subject.max' => 'الحد الأقصى لطول الموضوع هو 100 حرف.',
             'message.required' => 'حقل الرسالة مطلوب.',
             'message.string' => 'يجب أن تكون الرسالة نصًا.',
-            'message.mix' => 'الرسالة الا يقل عن 3 احرف.',
-            'message.min' => ' الرسالة الا يقل عن 200 احرف.',
+            'message.min' => 'الحد الأدنى لطول الرسالة هو 3 أحرف.',
+            'message.max' => 'الحد الأقصى لطول الرسالة هو 200 حرف.',
         ];
     }
 }
